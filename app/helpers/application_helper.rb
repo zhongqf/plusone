4fc751fb201aac5e75af81e7a5d50e7eedb27c7b
zhongqf <zhongqf@gmail.com>
2012-09-11 23:18:33 +0800

    new design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,14 @@
 module ApplicationHelper
   def flash_class(level)
-  case level
-  when :notice then 'info'
-  when :error then 'error'
-  when :alert then 'warning'
+  	case level
+  	when :notice then 'info'
+  	when :error then 'error'
+  	when :alert then 'warning'
+  	end
   end
+
+  def page_id
+  	"#{controller_path.parameterize}-#{action_name}"
   end
+  
 end
